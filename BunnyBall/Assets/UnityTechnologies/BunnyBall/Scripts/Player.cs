4bd36511975a0089edf227852aeee88adcbb53dd
Christopher Williams <chrsjwilliams@gmail.com>
2025-10-17 09:17:07 -0400

    Started making first level

--- BunnyBall/Assets/UnityTechnologies/BunnyBall/Scripts/Player.cs
+++ BunnyBall/Assets/UnityTechnologies/BunnyBall/Scripts/Player.cs
@@ -10,8 +10,29 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
+
+
+        float moveHorizontal = 0;// Input.GetAxis("Horizontal");
+        float moveVertical = 0;//Input.GetAxis("Vertical");
+        
+        if (Input.GetKey(KeyCode.A))
+        {
+            moveHorizontal = 1;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            moveHorizontal = -1;
+        }
+        
+        if (Input.GetKey(KeyCode.W))
+        {
+            moveVertical = 1;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            moveVertical = -1;
+        }
+        
         Vector3 forward = cameraTransform.forward;
         Vector3 right = cameraTransform.right;
         forward.y = 0;
